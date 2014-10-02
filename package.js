6a172191b9bd6f82d0db4cd4d55aea24bfff4181
Andrew Wilcox <andrew.wilcox@gmail.com>
2014-10-02 19:53:03 +0000

    Meteor appcache package as of 55621b

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  summary: "Enable the application cache in the browser",
+  version: "1.0.2-pre.0"
+});
+
+Package.on_use(function (api) {
+  api.use('webapp', 'server');
+  api.use('reload', 'client');
+  api.use('routepolicy', 'server');
+  api.use('underscore', 'server');
+  api.use('autoupdate', 'server', {weak: true});
+  api.add_files('appcache-client.js', 'client');
+  api.add_files('appcache-server.js', 'server');
+});
